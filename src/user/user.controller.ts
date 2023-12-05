a55607bf79392c3f09f2b53e685100bdb9263b22
taniamustika <taniasugiharto@gmail.com>
2023-12-05 22:47:16 +0700

    Buat Strategy Method untuk validasi token dan Nestjs Guard - completed

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Patch, UseGuards} from '@nestjs/common';
 import { JwtGuard } from 'src/auth/guards';
 import { User } from '@prisma/client'
+import { GetUser } from '../auth/decorator'
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
   @Get('me')
-  getMe(user: User) {
+  getMe(@GetUser() user: User) {
     return user;
   }
 
