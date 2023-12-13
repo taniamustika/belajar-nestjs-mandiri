ac7fa5e3aef7b390a15ee7dd64a64b5ba1e78b3e
taniamustika <taniasugiharto@gmail.com>
2023-12-13 19:21:58 +0700

    Add Swagger for Auth API

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,10 +1,13 @@
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger"
 
 export class AuthDto {
+  @ApiProperty({ example: 'tania@gmail.com', description:'Masukan email anda di sini' })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({ example: 'password123', description: 'Masukan password kamu di sini' })
   @IsString()
   @IsNotEmpty()
   password: string;
